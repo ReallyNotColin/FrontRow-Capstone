07290fd708651edc013b035eb66bef59844b65b4
amontes035 <153773042+amontes035@users.noreply.github.com>
2025-07-14 13:55:24 -0500

    Basic camera integration

--- frontend/app/(tabs)/scan.tsx
+++ frontend/app/(tabs)/scan.tsx
@@ -1,43 +1,65 @@
-import { ScrollView, StyleSheet } from 'react-native';
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+import { Platform, Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+export default function App() {
+  const [facing, setFacing] = useState<CameraType>('back');
+  const [permission, requestPermission] = useCameraPermissions();
 
-export default function Screen() {
+  if (!permission) 
+    // Camera permissions are still loading.
+    return <View />;
+
+  if (!permission.granted) {
+     // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="Grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
   return (
-    <ScrollView>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Scan</ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.divider} />
-      <ThemedView style={styles.text}>
-        <ThemedText>This screen is under construction.</ThemedText>
-      </ThemedView>
-    </ScrollView>
+    <View style={styles.container}>
+      <CameraView style={StyleSheet.absoluteFill} facing={facing} />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+          <Text style={styles.text}>Flip Camera</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    paddingTop: 60,
-    paddingBottom: 10,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
   },
-  divider: {
-    height: 2,
-    backgroundColor: '#E5E5EA',
-    marginBottom: 16,
-    width: '100%',
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 40,
+    left: 0,
+    right: 0,
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#00000088',
+    padding: 12,
+    borderRadius: 8,
   },
   text: {
-    backgroundColor: 'transparent',
-    paddingHorizontal: 24,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+    paddingBottom: Platform.OS === 'ios' ? 50 : 0,
   },
 });
